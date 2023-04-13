f081f132c1a72dbfe2052981f159dcf73940965d
danielvjacob <98068637+danielvjacob@users.noreply.github.com>
2023-04-13 17:18:46 -0500

    iterating through SQL table

---
+++ recipes.py
@@ -0,0 +1,16 @@
+# import mysql.connector
+# import pymysql
+
+
+# connection = pymysql.connect(host='localhost', user='nathan', password='password', database='TasteBud')
+# cursor = connection.cursor()
+
+
+# cursor.execute("SELECT * FROM recipes")
+# #to specify column, just switch the star with the col name
+# result = cursor.fetchall()
+
+# for row in result:
+#     print(row)
+    
+
