1c437a826970760340eee9dfb72a02609a488266
Nathan Stephani <nathanstephani12@gmail.com>
2023-04-13 15:25:58 -0500

    made recipe scraper interact with mysql database

--- scrape.py
+++ scrape.py
@@ -1,3 +1,5 @@
+import pymysql
+
 
 def alphaNum(s):
     for l in s:
@@ -27,5 +29,38 @@ for i in range(len(recipes)):
             newsteps.append(trim(step))
     recipes[i] = newsteps
 
-print(len(recipes))
-print(recipes[0])
+
+connection = pymysql.connect(host='localhost', user='nathan', password='password', database='TasteBud')
+
+cursor = connection.cursor()
+
+for i in range(1, 5):
+    str = ''
+    str += 'insert into recipes values ("'
+    str += recipes[i][0]
+    str += '","'
+    str += recipes[i][1]
+    str += '","'
+    str += recipes[i][2]
+    str += '","'
+    str += recipes[i][3]
+    str += '","'
+    str += recipes[i][4]
+    str += '","'
+    str += recipes[i][5]
+    str += '","'
+    str += recipes[i][6]
+    str += '","'
+    str += recipes[i][7]
+    str += '");'
+
+    cursor.execute(str)
+
+cursor.execute('select * from recipes')
+
+results = cursor.fetchall()
+
+for row in results:
+    print(row)
+
+connection.close()
