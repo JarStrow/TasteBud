411230325a0f20f2139a8461170015e9e1a94c19
Nathan Stephani <nathanstephani12@gmail.com>
2023-04-21 17:29:08 -0500

    Table for recipes in recipes.html

--- recipes.py
+++ recipes.py
@@ -1,14 +1,26 @@
+
+# Import from_db_cursor from prettytable library
+from prettytable import from_db_cursor
+# Import mysql connector
 import sqlite3
+# Import Error function from mysql.connector library
+# Declare host, database, user, password attributes using a dictionary
 
-connection = sqlite3.connect('recipes.db')
+# Check connectivity with the database
+db = sqlite3.connect('recipes.db')
 
-cursor = connection.cursor()
 
-cursor.execute("select recipe from recipes where ingredients like '% chicken %'")
-
-results = cursor.fetchall()
-
-for line in results:
-    print(line)
-
-connection.close()
+# Initiate cursor
+cursor = db.cursor()
+# Execute SQL query
+cursor.execute("SELECT recipe, ingredients, url FROM recipes")
+# Convert "gameRecords" table to a prettyTable
+mytable = from_db_cursor(cursor)
+# Generate the HTML code of the prettyTable using "get_html_string
+htmlCode = mytable.get_html_string(attributes={"class":"table"}, format=True)
+# Open prettyTable.html file
+fo = open("recipes.html", "w")
+# Write "htmlCode" to prettyTable.html
+fo.write(htmlCode)
+# Close prettyTable.html
+fo.close()
