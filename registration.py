4fb18f3da27937774757ba1d5e2848f1e889e415
Jarrett Strow <jarrett.c.strow-1@ou.edu>
2023-04-21 20:22:06 -0500

    fixed registration issues

--- registration.py
+++ registration.py
@@ -1,14 +1,23 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import sqlite3 as sql
+
 app = Flask(__name__, static_url_path='/static')
 
-conn = sql.connect('logins.db')
-print("Opened database successfully")
+# Create a function to check if a username already exists in the database
+@app.route('/check_username', methods=['POST'])
+def check_username(username):
+    with sql.connect('logins.db') as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM logins WHERE username=?", (username,))
+        user = cur.fetchone()
+        if user is None:
+            # Username is available
+            return True
+        else:
+            # Username is already taken
+            return False
 
-conn.execute('CREATE TABLE IF NOT EXISTS logins (firstname TEXT NOT NULL, lastname TEXT NOT NULL, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL)')
-print("Table created successfully")
-conn.close()
-
+# Define the routes for your app
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -41,30 +50,35 @@ def login():
 def registration():
     return render_template('registration.html')
 
-@app.route('/addrec',methods=['POST','GET'])
-def addrec():
-    if request.method == 'POST':
-        try:
-            firstname = request.form['firstname']
-            lastname = request.form['lastname']
-            username = request.form['username']
-            password  = request.form['password']
-        
-            with sql.connect('logins.db') as con:
-                cur = con.cursor()
+# Handle the registration form submission
+@app.route('/register', methods=['POST'])
+def register():
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    username = request.form['username']
+    password = request.form['password']
+    error = None
 
-                cur.execute("INSERT INTO logins (firstname, lastname, username, password) VALUES (?,?,?,?)", (firstname, lastname, username, password))
-                print("Added to table successfully")
+    if not firstname:
+        error = 'First name is required.'
+    elif not lastname:
+        error = 'Last name is required.'
+    elif not username:
+        error = 'Username is required.'
+    elif not password:
+        error = 'Password is required.'
+    elif check_username(username) == False:
+        error = 'Username already exists.'
 
-                con.commit()
-                msg = "Record added"
-        except:
-            con.rollback()
-            msg = "error in insert operation"
-        
-        finally:
-            con.close()
-            return render_template("index.html")
+    if error:
+        return render_template('registration.html', error=error)
+    
+    # Insert the new user into the database
+    with sql.connect('logins.db') as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO logins (firstname, lastname, username, password) VALUES (?, ?, ?, ?)", (firstname, lastname, username, password))
+        con.commit()
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5500)
